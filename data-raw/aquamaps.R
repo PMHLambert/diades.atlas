fba8de30345364a29c75390702b8a669535ed884
Patrick Lambert <patrick.mh.lambert@inrae.fr>
2024-10-07 15:24:23 +0200

    ESSAI

--- data-raw/aquamaps.R
+++ data-raw/aquamaps.R
@@ -21,7 +21,6 @@
 # # initial run-once step required to install remote db locally
 # download_db(force = TRUE)
 
- #essai
 
 # active the connection to the downloaded database:
 default_db("sqlite")
